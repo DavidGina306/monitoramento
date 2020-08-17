20d955a1cea312eee3feb19f8cf4cd5e492b4093
Daniel de Sá <contato@danieldesa.com>
2020-08-16 21:52:00 -0400

    Adição do navbar e correção de tamanho do layout

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -1,39 +1,42 @@
 <template>
-    <b-container class="mt-5 bg-light border p-2 rounded">
-        <b-row>
-            <div class="col-sm-6 mt-negative col-12 mb-5 mb-md-0">
-                <div class="bg-info rounded  p-2 text-white text-center font-weight-bold">
-                    SEPARANDO ({{pedSeparacao.length}})
+    <div>
+        <nav-bar/>
+        <b-container class="mt-4 bg-light border p-2 rounded">
+            <b-row>
+                <div class="col-sm-6 mt-negative col-12 mb-5 mb-md-0">
+                    <div class="bg-info rounded  p-2 text-white text-center font-weight-bold">
+                        SEPARANDO ({{pedSeparacao.length}})
+                    </div>
+                    <div v-if="pedSeparacao.length > 0" class="table-responsive vh-75 overflow-hidden">
+                        <table class="table rounded-0 overflow-auto">
+                            <thead>
+                            <tr>
+                                <th class="border-0">NRO. PEDIDO</th>
+                                <th class="border-0">CLIENTE</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr v-for="item in  pedSeparacao" :key="item.numped">
+                                <td> {{item.numped}}</td>
+                                <td>{{item.cliente}}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <h4 class="text-center p-2 text-black-50" v-else>Sem produtos</h4>
                 </div>
-                <div v-if="pedSeparacao.length > 0" class="table-responsive vh-80 overflow-hidden">
-                    <table class="table rounded-0 overflow-auto">
-                        <thead>
-                        <tr>
-                            <th class="border-0">NRO. PEDIDO</th>
-                            <th class="border-0">CLIENTE</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr v-for="item in  pedSeparacao" :key="item.numped">
-                            <td> {{item.numped}}</td>
-                            <td>{{item.cliente}}</td>
-                        </tr>
-                        </tbody>
-                    </table>
+                <div class="col-12 mt-negative col-sm-6 col-md-4 offset-0 offset-md-2">
+                    <div class="bg-success rounded p-2 text-white text-center font-weight-bold">
+                        <i class="fas fa-people-carry"/> DISPONÍVEL NA EXPEDIÇÃO ({{pedExpedicao.length}})
+                    </div>
+                    <div v-if="pedExpedicao.length > 0" class="overflow-hidden vh-75">
+                        <info-cards v-for="(item,idx) in pedExpedicao.slice(0,4)" :key="idx" :item="item"/>
+                    </div>
+                    <h4 class="text-center p-2 text-black-50" v-else>Sem produtos</h4>
                 </div>
-                <h4 class="text-center p-2 text-black-50" v-else>Sem produtos</h4>
-            </div>
-            <div class="col-12 mt-negative col-sm-6 col-md-4 offset-0 offset-md-2">
-                <div class="bg-success rounded p-2 text-white text-center font-weight-bold">
-                    <i class="fas fa-people-carry"/> DISPONÍVEL NA EXPEDIÇÃO ({{pedExpedicao.length}})
-                </div>
-                <div v-if="pedExpedicao.length > 0" class="overflow-hidden vh-80">
-                    <info-cards v-for="(item,idx) in pedExpedicao.slice(0,4)" :key="idx" :item="item"/>
-                </div>
-                <h4 class="text-center p-2 text-black-50" v-else>Sem produtos</h4>
-            </div>
-        </b-row>
-    </b-container>
+            </b-row>
+        </b-container>
+    </div>
 </template>
 <script>
     import InfoCards from './InfoCards.vue'
@@ -93,8 +96,8 @@
     }
 </script>
 <style scoped>
-    .vh-80 {
-        height: 80vh;
+    .vh-75 {
+        height: 75vh;
     }
 
     .mt-negative {
